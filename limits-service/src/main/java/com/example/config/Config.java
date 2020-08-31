f8088c5b99951999140c498c85121a4bf03b26e4
SamirAllahverdi <a_nazim2007@mail.ru>
2020-08-31 22:42:25 +0400

    first commit

---
+++ limits-service/src/main/java/com/example/config/Config.java
@@ -0,0 +1,19 @@
+package com.example.config;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+@Getter
+@Setter
+public class Config {
+
+    private int maximum;
+    private int minimum;
+
+
+}
